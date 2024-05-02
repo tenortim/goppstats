81c79568c34afd6575be6f4a772ac3e88e3f0729
Tim Wright <tenortim@gmail.com>
2024-05-02 10:58:50 -0700

    Tiny (benign) fix
        The InfluxDBv2 API does not return an error type so the check for
        'err != nil' that was copied from the v1 code was unnecessary and has
        been deleted.

--- influxdbv2.go
+++ influxdbv2.go
@@ -52,9 +52,6 @@ func (s *InfluxDBv2Sink) Init(cluster *Cluster, config *tomlConfig, ci int) erro
 			log.Errorf("InfluxDB async write error for cluster %s: %s\n", cluster, err.Error())
 		}
 	}()
-	if err != nil {
-		return fmt.Errorf("failed to create InfluxDBv2 client - %v", err.Error())
-	}
 
 	s.exports = newExportMap(config.Global.LookupExportIds)
 	return nil
